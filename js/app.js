f351fd0d5cb7c7c550bd2369d0c23f04a1c991bc
luisNieb <130430946+luisNieb@users.noreply.github.com>
2023-04-17 22:46:17 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const no=document.querySelector("#no")
+const buenoX=document.querySelector("#buenoX")
+const en=document.querySelector("#en")
+const a=document.querySelector("#a")
+const de=document.querySelector("#de")
+const de20=document.querySelector("#de20")
+const personaPaga=document.querySelector("#personaPaga")
+const elPAgo=document.querySelector("#elPAgo")
+const fechaPago=document.querySelector("#fechaPago")
+const cantidadDe=document.querySelector("#cantidadDe")
+const serie=document.querySelector("#serie")
+const interes=document.querySelector("#interes")
+const nombre=document.querySelector("#nombre")
+const direccion=document.querySelector("#Direccion")
+const Poblacion=document.querySelector("#Poblacion")    
+const tel=document.querySelector("#tel")
+const firma=document.querySelector("#firma")
+const boton=document.querySelector("#boton")
+
+//hola esto no jala 
+const fechaER = /^\d{2}\/\d{2}\/\d{4}$/;
